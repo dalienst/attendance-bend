bb5578819d5a9cd241be1ba6f3ede2af6723f8d5
dalienst <oduordalienst@gmail.com>
2022-11-30 13:02:02 +0300

    set production

--- attendance/settings/staging.py
+++ attendance/settings/staging.py
@@ -1,9 +1,10 @@
 from attendance.settings.base import ALLOWED_HOSTS
 
-ALLOWED_HOSTS += [
+ALLOWED_HOSTS = [
     "http://localhost:3000",
     "localhost",
     "127.0.0.1",
+    "https://projectattend.netlify.app/",
 ]
 
 DEBUG = True
